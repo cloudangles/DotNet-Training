c19c36c66e0984a0cabd760a7e89ee21f000e4d4
SathwikaGopishetty <111847859+SathwikaGopishetty@users.noreply.github.com>
2022-08-23 17:03:16 +0530

    second commit

---
+++ practice/oopspractise/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+interface IFigure
+{
+int Dimension { get; set; } 
+double Area();
+double Perimeter();
+}
+class Circle : IFigure
+{
+    private int _Radius;
+    public int Dimension
+    {
+        get
+        {
+            return _Radius;
+        }
+        set
+        {
+            _Radius = value;
+        }
+    }
+    public double Area()
+    {
+        return Math.PI * _Radius * _Radius;
+    }
+    public double Perimeter()
+    {
+        return 2 * Math.PI * _Radius;
+    }
+    class Square : IFigure
+    {
+        private int _Side;
+        public int Dimension
+        {
+            get
+            {
+                return _Side;
+            }
+            set
+            {
+                _Side = value;
+            }
+        }
+        public double Area()
+        {
+            return _Side * _Side;
+        }
+        public double Perimeter()
+        {
+            return 4 * _Side;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFigure fig = null;
+            if (args[0] == "S")
+                fig = new Square();
+            if (args[0] == "C")
+                fig = new Circle();
+            fig.Dimension = 10;
+            Console.WriteLine(fig.Area());
+            Console.WriteLine(fig.Perimeter());
+        }
+    }
+}
---
+++ practice/practice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> lst = new List<string>();
+        Console.WriteLine(lst.Capacity);
+        lst.Add("s1");
+        lst.Add("s2");
+        lst.Add("s3");
+        lst.Insert(2, "s1.5");
+        lst.Remove("s2");
+        lst.Remove("s1.5");
+        Console.WriteLine("The elements count is :" +lst.Count);
+        Console.WriteLine("the capacity is : " +lst.Capacity);
+        Console.WriteLine("The string s1 exists : " + lst.Contains("s1"));
+        foreach (string s in lst)
+        {
+            Console.WriteLine("The elememts in string are " +s);
+        }
+        string[] arString = lst.ToArray();
+    }
+}
